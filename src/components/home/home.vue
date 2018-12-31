66aab099a3228280ec575771f9b5107dd603f9f9
zhjsong <492992198@qq.com>
2018-12-31 21:55:39 +0800

    '路由主键配置'

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -21,7 +21,8 @@
     <el-container>
       <el-aside width="200px" class="aside">
            <el-menu
-           unique-opened='true'
+           :unique-opened='true'
+           :router='true'
            class="el-menu-vertical-demo"
            >
       <el-submenu index="1">
@@ -30,7 +31,7 @@
           <span>用户管理</span>
         </template>
 
-          <el-menu-item index="1-1">
+          <el-menu-item index="users">
               <template slot="title">
           <i class="el-icon-menu"></i>
           <span>用户列表</span>
@@ -121,7 +122,9 @@
       </el-submenu>
     </el-menu>
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+        <router-view></router-view>
+      </el-main>
    </el-container>
  </el-container>
 </template>
---
+++ src/components/users/users.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <div>三生三世</div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+
+</style>
+
