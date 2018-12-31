d2267a12d74c7c7eef280a39c14d709de00ea2e0
zhjsong <492992198@qq.com>
2018-12-31 14:23:24 +0800

    '换'

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-// 导入elemenyui组件库
+// 导入elemenyui组件库123
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
